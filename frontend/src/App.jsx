6bbc5d9d76234459fc81c42d20dc00057e5c429f
Gabriel Moreira <gabrielms00777@gmail.com>
2025-05-19 11:45:29 -0300

    tailwind finalmente funcionando, depois de muita dor de cabeça

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-blue-600 text-white text-4xl flex justify-center items-center">
+      Tailwind funcionando!
+    </div>
+  );
+}
